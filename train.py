3a23265112a8075a02867865fd705f4454e2cf97
Sarzil <sarzilh701@gmail.com>
2022-04-20 00:48:35 +0600

    Updated

--- train.py
+++ train.py
@@ -1,46 +1,80 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+import tensorflow_addons as tfa
+import keras_tuner as kt
 
 # Creating a model with TemsprFlow's Sequential API for offensive text sentiment analysis
 def create_and_compile_model(hp):
 
     embedding_dim = hp.Int("Embedding dim", min_value=64, max_value=512, step=64)
     dense_1_nodes = hp.Int("dense_1_nodes", min_value=8, max_value=128, step=8)
-    
+
     inputs = tf.keras.layers.Input(shape=(max_length))
-    x = tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length)(inputs)
-    x = tf.keras.layers.GlobalAveragePooling1D()(x)
-    x = tf.keras.layers.Dense(dense_1_nodes, activation='relu')(x)
-    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
-    
-    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
-    
-    learning_rate = hp.Choice("learning_rate", [0.001, 0.0001])
-    
-    model.compile(
-    loss = tf.keras.losses.BinaryCrossentropy(),
-    optimizer = tf.keras.optimizers.Adam(learning_rate),
-    metrics = [tf.keras.metrics.BinaryAccuracy(), tfa.metrics.F1Score(num_classes=1, threshold=0.5)]
-    
+    x = tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length)(
+        inputs
     )
-    
+    x = tf.keras.layers.GlobalAveragePooling1D()(x)
+    x = tf.keras.layers.Dense(dense_1_nodes, activation="relu")(x)
+    outputs = tf.keras.layers.Dense(1, activation="sigmoid")(x)
+
+    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
+
+    learning_rate = hp.Choice("learning_rate", [0.001, 0.0001])
+
+    model.compile(
+        loss=tf.keras.losses.BinaryCrossentropy(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate),
+        metrics=[
+            tf.keras.metrics.BinaryAccuracy(),
+            tfa.metrics.F1Score(num_classes=1, threshold=0.5),
+        ],
+    )
+
     return model
 
 
-def hyperparameter_tuning(padded_training_sequences, labels_for_training, validation_data):
+def hyperparameter_tuning(
+    padded_training_sequences, labels_for_training, validation_data
+):
 
-    tuner = kt.Hyperband(create_and_compile_model,
-                         objective=kt.Objective("f1_score", direction="max"),
-                         max_epochs=10,
-                         factor=10,
-                         directory='kt_dir',
-                         project_name="otsa_")
+    tuner = kt.Hyperband(
+        create_and_compile_model,
+        objective=kt.Objective("f1_score", direction="max"),
+        max_epochs=10,
+        factor=10,
+        directory="kt_dir",
+        project_name="otsa_",
+    )
 
-    callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_f1_score', min_delta=0.0001, patience=5)]
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(
+            monitor="val_f1_score", min_delta=0.0001, patience=5
+        )
+    ]
 
-    tuner.search(padded_training_sequences, labels_for_training, validation_data=(padded_validation_sequences, labels_for_validation), epochs=50, callbacks=callbacks)
-    
+    tuner.search(
+        padded_training_sequences,
+        labels_for_training,
+        validation_data=(padded_validation_sequences, labels_for_validation),
+        epochs=50,
+        callbacks=callbacks,
+    )
+
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
     model = tuner.hypermodel.build(best_hps)
-    
+
     return model, tuner
 
-def train(model, )
+def train():
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(
+            monitor="val_f1_score", min_delta=0.0001, patience=5
+        )
+    ]
+    num_epochs = 30
+    batch_size = 64
+
+    history = model.fit(padded_training,labels_for_training, epochs=num_epochs, batch_size=batch_size,verbose=1, callbacks=callbacks)
+    
+    return model
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 # Downloading the datasets
 def download_and_join_datasets():
-    
+
     import os, requests, zipfile, glob
     import pandas as pd
-    
+
     datasets_zip = "cyberbullying_dataset-mendeley_data.zip"
     datasets_dir = "cyberbully_datasets/"
     datasets_url = "https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/jf4pzyvnpj-1.zip"
@@ -22,44 +24,69 @@ def download_and_join_datasets():
         with zipfile.ZipFile(datasets_zip) as zf:
             zf.extractall(datasets_dir)
             print("The Dataset has been extracted!")
-    if not os.path.exists(os.path.join(datasets_dir, "Cyberbully_dataset_combined.csv")):
+    if not os.path.exists(
+        os.path.join(datasets_dir, "Cyberbully_dataset_combined.csv")
+    ):
         print("Joining the datasets together...")
         datasets = glob.glob(os.path.join(datasets_dir, "*.csv"))
-        dataframe = pd.concat(list(map(lambda dataset: pd.read_csv(dataset)[["Text", "oh_label"]], datasets)),axis=0)    
-        dataframe = dataframe.rename(columns={"oh_label":"Offensive"})
+        dataframe = pd.concat(
+            list(
+                map(
+                    lambda dataset: pd.read_csv(dataset)[["Text", "oh_label"]], datasets
+                )
+            ),
+            axis=0,
+        )
+        dataframe = dataframe.rename(columns={"oh_label": "Offensive"})
         print("Dataset Joined Successfully. Saving it...")
-        dataframe.to_csv(os.path.join(datasets_dir, "Cyberbully_dataset_combined.csv"), index=False)
-        print(r"Saved 'Cyberbully_dataset_combined.csv'. The dataset is also availabe on Kaggle: https://www.kaggle.com/datasets/prmethus/mendeleys-cyberbully-datasets-combined?select=Cyberbully_dataset_combined.csv")
-        
-    print(f"\nDataset path: {os.path.join(datasets_dir, 'Cyberbully_dataset_combined.csv')}")
+        dataframe.to_csv(
+            os.path.join(datasets_dir, "Cyberbully_dataset_combined.csv"), index=False
+        )
+        print(
+            r"Saved 'Cyberbully_dataset_combined.csv'. The dataset is also availabe on Kaggle: https://www.kaggle.com/datasets/prmethus/mendeleys-cyberbully-datasets-combined?select=Cyberbully_dataset_combined.csv"
+        )
 
-            
+    print(
+        f"\nDataset path: {os.path.join(datasets_dir, 'Cyberbully_dataset_combined.csv')}"
+    )
+
+
 # Defining a function for tokenizin and padding words in a sentence
-def tokenize_and_pad(texts, tokenizer, maxlen=100, padding='post', truncating='post'):
-    
+def tokenize_and_pad(texts, tokenizer, maxlen=100, padding="post", truncating="post"):
+
     sequences = tokenizer.texts_to_sequences(texts)
-    padded_sequences = pad_sequences(sequences, maxlen, padding=padding, truncating=truncating)
-    
+    padded_sequences = pad_sequences(
+        sequences, maxlen, padding=padding, truncating=truncating
+    )
+
     return padded_sequences
 
 
 def stratified_split(dataset):
-        
+
     from sklearn.model_selection import StratifiedShuffleSplit
-    
-    dataset = dataset.reset_index() # Required for Stratified Split.
-    
+
+    dataset = dataset.reset_index()  # Required for Stratified Split.
+
     split1 = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
     split2 = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=0)
-    
-    for train_set_index, validation_test_set_index in split1.split(dataset["Text"], dataset["Offensive"]):
-        train_set = dataset.loc[train_set_index,["Text", "Offensive"]]
-        validation_test_set = dataset.loc[validation_test_set_index,["Text", "Offensive"]]
-    
+
+    for train_set_index, validation_test_set_index in split1.split(
+        dataset["Text"], dataset["Offensive"]
+    ):
+        train_set = dataset.loc[train_set_index, ["Text", "Offensive"]]
+        validation_test_set = dataset.loc[
+            validation_test_set_index, ["Text", "Offensive"]
+        ]
+
     validation_test_set = validation_test_set.reset_index()
-    
-    for validation_set_index, test_set_index in split2.split(validation_test_set["Text"], validation_test_set["Offensive"]):
-        validation_set = validation_test_set.loc[validation_set_index,["Text", "Offensive"]]
-        test_set = validation_test_set.loc[test_set_index,["Text", "Offensive"]]
-        
+
+    for validation_set_index, test_set_index in split2.split(
+        validation_test_set["Text"], validation_test_set["Offensive"]
+    ):
+        validation_set = validation_test_set.loc[
+            validation_set_index, ["Text", "Offensive"]
+        ]
+        test_set = validation_test_set.loc[test_set_index, ["Text", "Offensive"]]
+
     return train_set, validation_set, test_set
